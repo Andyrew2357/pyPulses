401cf416bb7372ceef16d93c78263ec5b739296a
Andyrew2357 <andyrew2357@gmail.com>
2025-04-27 19:30:38 -0400

    added derivatives to cq calculation

--- pyPulses/routines/gap_extractor.py
+++ pyPulses/routines/gap_extractor.py
@@ -4,14 +4,23 @@ from typing import Optional
 from ..utils import curves
 
 class GapExtractor():
-    def __init__(self, ct: Optional[float], cb: Optional[float], 
-                 chi_g: Optional[float], chi_b: Optional[float], 
-                 omega: Optional[float]):
+    def __init__(self, ct   : Optional[float] = None, 
+                 cb         : Optional[float] = None, 
+                 chi_g      : Optional[float] = None, 
+                 chi_b      : Optional[float] = None, 
+                 omega      : Optional[float] = None,
+                 var_chi_g  : Optional[float] = None, 
+                 var_chi_b  : Optional[float] = None):
         self.ct = ct
         self.cb = cb
         self.chi_g = chi_g
         self.chi_b = chi_b
         self.omega = omega
+
+        self.var_chi_g = var_chi_g
+        self.var_chi_b = var_chi_b
+
+        self.q = 1.60217663e-19
 
         self.inv_coeff_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), r'ztan_inv_coeff.npy'
@@ -35,7 +44,7 @@ class GapExtractor():
         self.ztan_invp = curves.pchip_dval_from_params(*pchip_coeff)
         np.save(self.inv_coeff_path, pchip_coeff, allow_pickle = True)
 
-    def calc_cq_AR(self, chi_re, chi_im = 0, mode = 'tl_model'):
+    def calc_cq_AR(self, chi_re, chi_im = 0, mode = 'tl_model', derivatives = False):
         """
         Back out the quantum capacitance and AR (defined by the transmission 
         line model) from raw capacitance data.
@@ -84,7 +93,77 @@ class GapExtractor():
                 # back out AR from x = (ωARcq(ct+cb)/8(cq+ct+cb))^1/2                
                 AR = sign_R*(8*(cq + cT)/(self.omega*cq*cT))*x**2
 
-                print(f"chi_re={chi_re}, chi_im={chi_im}, chi_g={self.chi_g}, chi_b={self.chi_b}, ct={self.ct}, cb={self.cb}, cq={cq}, AR={AR}, x={x}, F_re={F_re}, F_im={F_im}")
+                if derivatives:
+
+                    # the pseudo-analytic version of this doesn't seem to work
+                    # for some reason, so I'm just doing it numerically.
+                    if False:
+                        # see the document I wrote to understand what's happening here
+
+                        if abs(x) < 1e-3:
+                            dF_re_dX = dF_im_dX = 0
+                            dF_re_dY = dF_im_dY = 0
+                        else:
+                            gp = self.ztan_invp(ztan)
+                            dx_dX = -(Y/X**2)*gp
+                            dx_dY = (1/X)*gp
+
+                            Fp_re = (1 - F_re)/x + 4*x*F_re*F_im
+                            Fp_im = (2/denom)*(np.cos(2*x) - np.cosh(2*x)) - \
+                                                    F_im/x + 4*x*F_im*F_im
+
+                            dF_re_dX = Fp_re*dx_dX
+                            dF_im_dX = Fp_im*dx_dX
+                            dF_re_dY = Fp_re*dx_dY
+                            dF_im_dY = Fp_im*dx_dY
+
+                        D = cT/((X - F_re)**2 + (Y - F_im)**2)
+                        r = 2*cq/cT
+                        a = 1 - r
+                        b = 1 + r
+                        c = r - 2
+
+                        dcq_dX = D*(a*(X*dF_re_dX + F_re) + b*Y*dF_im_dX + c*X + \
+                                                r*(F_re*dF_re_dX + F_im*dF_im_dX))
+                        dcq_dY = -D*(a*(Y*dF_im_dY + F_im) + b*X*dF_re_dY + c*Y + \
+                                                r*(F_re*dF_re_dY + F_im*dF_im_dY))
+
+
+                        d = 1/(self.chi_b - self.chi_g)
+
+                        dX_dchi_r = d
+                        dY_dchi_r = 0
+                        dX_dchi_i = 0
+                        dY_dchi_i = d
+
+                        dX_dchi_g = -d - d*d*(self.chi_g - chi_re)
+                        dY_dchi_g = d*d*chi_im
+                        dX_dchi_b = d*d*(self.chi_g - chi_re)
+                        dY_dchi_b = -d*d*chi_im
+
+                        dcq_dchi_r = dcq_dX*dX_dchi_r + dcq_dY*dY_dchi_r
+                        dcq_dchi_i = dcq_dX*dX_dchi_i + dcq_dY*dY_dchi_i
+                        dcq_dchi_g = dcq_dX*dX_dchi_g + dcq_dY*dY_dchi_g
+                        dcq_dchi_b = dcq_dX*dX_dchi_b + dcq_dY*dY_dchi_b
+
+                    delta = 1e-6
+                    dcq_dchi_r = (self.calc_cq_AR(chi_re + delta, chi_im, 
+                        mode = 'tl_model', derivatives = False)[0] - cq)/delta
+                    
+                    dcq_dchi_i = (self.calc_cq_AR(chi_re, chi_im + delta, 
+                        mode = 'tl_model', derivatives = False)[0] - cq)/delta
+                    
+                    chi_g = self.chi_g
+                    self.chi_g += delta
+                    dcq_dchi_g = (self.calc_cq_AR(chi_re, chi_im, 
+                        mode = 'tl_model', derivatives = False)[0] - cq)/delta
+                    self.chi_g = chi_g
+
+                    chi_b = self.chi_b
+                    self.chi_b += delta
+                    dcq_dchi_b = (self.calc_cq_AR(chi_re, chi_im, 
+                        mode = 'tl_model', derivatives = False)[0] - cq)/delta
+                    self.chi_b = chi_b
 
             # low frequency limit of the transmission line calculation
             case 'low_freq':
@@ -93,7 +172,39 @@ class GapExtractor():
                                          (chi_re - self.chi_b)
                 AR = 0
 
+                if derivatives:
+                    cT = self.ct + self.cb
+                    dcq_dchi_r = cT*(self.chi_b - self.chi_g)/(chi_re - self.chi_b)**2
+                    dcq_dchi_i = 0
+                    dcq_dchi_g = cT/(self.chi_b - chi_re)
+                    dcq_dchi_b = cq/(chi_re - self.chi_b)
+
             case _:
                 raise ValueError(f"{mode} is not a valid mode for calc_cq_AR")
 
+        if derivatives:
+            return cq, AR, (dcq_dchi_r, dcq_dchi_i, dcq_dchi_g, dcq_dchi_b)
+
         return cq, AR
+
+    def calc_gap(self, vt, vb, cq):
+        """Integrate to determine the gap in eV"""
+
+        mt = (1 + cq/self.ct)**-1
+        mb = (1 + cq/self.cb)**-1
+
+        return self.q*(np.trapezoid(mt, vt) + np.trapezoid(mb, vb))
+    
+    def calc_gap_uncertainty(self, vt, vb, cq, derivatives, cov_chi):
+        """
+        Calculate the uncertainty for an integrated gap using error propagation
+        """
+
+        # derivatives = dcq_dchi_r, dcq_dchi_i, dcq_dchi_g, dcq_dchi_b
+        mt2 = (1 + cq/self.ct)**-2
+        mb2 = (1 + cq/self.cb)**-2
+        dmt_dchi_r, dmt_dchi_i, dmt_dchi_g, dmt_dchi_b = [-d*mt2/self.ct for d in derivatives]
+        dmb_dchi_r, dmb_dchi_i, dmb_dchi_g, dmb_dchi_b = [-d*mb2/self.cb for d in derivatives]
+        chi_g_part = self.var_chi_g*(np.trapezoid(dmt_dchi_g, vt) + np.trapezoid(dmb_dchi_g, vb))**2
+        chi_b_part = self.var_chi_b*(np.trapezoid(dmt_dchi_b, vt) + np.trapezoid(dmb_dchi_b, vb))**2
+
